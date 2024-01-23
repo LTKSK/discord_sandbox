ae61cf7baf54450f5e2f1d79048d8a49b606eaf8
LTKSK <ltksk0714@gmail.com>
2024-01-23 23:02:19 +0900

    configの設定を変更して実行できるようにした

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,19 @@ const commands = [
 
 console.log(process.env.TOKEN);
 
-// const rest = new REST({ version: "10" }).setToken(TOKEN);
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
 
-// try {
-//   console.log("Started refreshing application (/) commands.");
+if (TOKEN !== undefined && CLIENT_ID !== undefined) {
+  const rest = new REST({ version: "10" }).setToken(TOKEN);
 
-//   await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+  try {
+    console.log("Started refreshing application (/) commands.");
 
-//   console.log("Successfully reloaded application (/) commands.");
-// } catch (error) {
-//   console.error(error);
-// }
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+}
